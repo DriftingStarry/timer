6663117199182a78b618afafd3c8762bc3b9678b
DriftingStarry <doubichenzi@126.com>
2024-02-28 20:52:04 +0800

    finished

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from datetime import datetime
-nowdate = datetime.now()
-targetdate = datetime(2024,7,29)
-r = targetdate - nowdate
-print(r)
+from tkinter import *
+def gettime():
+    nowdate = datetime.now()
+    targetdate = datetime(2024,7,29)
+    r = targetdate - nowdate
+    r = str(r)[:-7]
+    return nowdate.strftime('%Y-%m-%d %H:%M:%S'),targetdate,r
+
+root = Tk()
+root.geometry('200x200')
+nowtime = Label(root,text='')
+targetdate = Label(root,text='')
+ra = Label(root,text='')
+nowtime.pack(pady=5)
+targetdate.pack(pady=5)
+ra.pack(pady=5)
+def update():
+    n,t,r = gettime()
+    nowtime["text"] = '当前时间:'+str(n) 
+    targetdate["text"] = '美丽的假期最晚将在\n'+str(t)+'\n开始'
+    ra["text"] = '就只有\n'+r+'\n这么点时间了'
+    nowtime.after(1000,update)
+update()
+root.mainloop()
