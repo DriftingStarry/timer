b3663e2403ecb6690f3d8e0655d54fcb93f95c39
DriftingStarry <doubichenzi@126.com>
2024-03-10 19:07:08 +0800

    v1.1.0

--- main.py
+++ main.py
@@ -1,26 +1,68 @@
 from datetime import datetime
-from tkinter import *
-def gettime():
-    nowdate = datetime.now()
-    targetdate = datetime(2024,7,29)
-    r = targetdate - nowdate
-    r = str(r)[:-7]
-    return nowdate.strftime('%Y-%m-%d %H:%M:%S'),targetdate,r
+import tkinter as tk
 
-root = Tk()
-root.geometry('200x200')
-root.title('Timer v1.0.1')
-nowtime = Label(root,text='')
-targetdate = Label(root,text='')
-ra = Label(root,text='')
-nowtime.pack(pady=5)
-targetdate.pack(pady=5)
-ra.pack(pady=5)
+def cultime(start,end):
+    gap = end - start
+    gap = str(gap)[:-7]
+    return gap
+
+def getnow():
+    nowtime = datetime.now()
+    nowtime = nowtime.strftime('%Y-%m-%d %H:%M:%S')
+    return nowtime
+
+def get_target():
+    user_input = entry.get()
+    # input format: 'year month day'
+    try:
+        y, m, d = map(int,user_input.split())
+    except:
+        y, m, d = 2024, 7, 29
+    targetdate = datetime(y, m, d)
+    return targetdate
 def update():
-    n,t,r = gettime()
-    nowtime["text"] = '当前时间:'+str(n) 
-    targetdate["text"] = '美丽的假期最晚将在\n'+str(t)+'\n开始'
-    ra["text"] = '就只有\n'+r+'\n这么点时间了'
-    nowtime.after(1000,update)
+    nowtime = getnow()
+    targetdate = get_target()
+    timegap = cultime(datetime.now(), targetdate)
+
+    nowtime_L["text"] = nowtime
+    targetdate_L["text"] = targetdate
+    timegap_L["text"] = timegap
+    nowtime_L.after(1000,update)
+
+
+root = tk.Tk()
+root.title('Timer v1.1.0') #title
+root.geometry('300x300') #size
+
+t1 = tk.Label(text='当前时间')
+t1.pack()
+
+nowtime = getnow()
+nowtime_L = tk.Label(root,text=nowtime)
+nowtime_L.pack()
+
+t2 = tk.Label(text = '请输入目标时间\n例如 2024 7 29')
+t2.pack()
+
+entry = tk.Entry(root)
+et = tk.StringVar()
+et.set('2024 7 29')
+entry["textvariable"] = et
+entry.pack()
+
+t3 = tk.Label(text='识别到的时间, 未识别到则默认2024 7 29')
+t3.pack()
+
+targetdate = datetime(2024, 7, 29)
+targetdate_L = tk.Label(root, text = targetdate)
+targetdate_L.pack()
+
+t4 = tk.Label(text='距离目标时间还有')
+t4.pack()
+timegap = cultime(datetime.now(),targetdate)
+timegap_L = tk.Label(root,text=timegap)
+timegap_L.pack()
+
 update()
 root.mainloop()
